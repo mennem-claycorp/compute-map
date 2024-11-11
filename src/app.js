afa663678953bd1b0d8c4f9f96f84b50e3619386
Vitalii Shytikov <v.shytikov@gmail.com>
2024-11-11 10:09:03 +0100

    move styles and templates to a separate files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./styles/map.css";
 // import data from "./locations.json";
+import filterTypeTemplate from "./templates/filter-type.html";
 
 import chargerIcon from "../static/charger.svg";
 import locationIcon from "../static/location_on.svg";
@@ -22,173 +24,11 @@ const classnames = {
 };
 
 export class OSMap extends HTMLElement {
-  static defaultStylesAdded = false;
-
   constructor() {
     super();
-    this.defaultStyles = `
-      <style>
-        os-map {
-          #map {
-            width: 100%;
-            height: 1000px;
-          }
-
-          .dropdown {
-            position: relative;
-            width: 254px;
-            margin-bottom: 20px;
-            font-family:
-              DIN Offc Pro Medium,
-              Arial,
-              sans-serif;
-          }
-
-          .dropdown-toggle {
-            width: 176px;
-            padding: 10px 0;
-            background: white;
-            border-bottom: 2px solid #212121;
-            border-top: none;
-            border-right: none;
-            border-left: none;
-            padding-right: 10px;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            cursor: pointer;
-            font-size: 14px;
-          }
-
-          .dropdown-menu {
-            position: absolute;
-            top: 100%;
-            left: 0;
-            width: 200px;
-            background: white;
-            border: 1px solid #212121;
-            display: none;
-            padding: 10px;
-            z-index: 1;
-          }
-
-          .dropdown-menu.show {
-            display: block;
-          }
-
-          .arrow.rotate {
-            transform: rotate(180deg);
-            transition: transform 0.2s ease;
-          }
-
-          .dropdown-item {
-            display: flex;
-            align-items: center;
-            gap: 10px;
-            padding: 8px 0;
-            cursor: pointer;
-          }
-
-          .badge {
-            padding: 5px 12px;
-            border-radius: 20px;
-            color: white;
-            font-size: 14px;
-            font-weight: 500;
-            text-transform: uppercase;
-          }
-
-          .enterprise {
-            background-color: #212121;
-          }
-
-          .quantum {
-            background-color: #1476e8;
-          }
-
-          .ai {
-            background-color: #28a745;
-          }
-
-          .colocation {
-            background-color: #4e2751;
-          }
-
-          .hyperscale {
-            color: #212121;
-            background-color: #ffc107;
-          }
-
-          input[type="checkbox"] {
-            width: 16px;
-            height: 16px;
-            margin: 0;
-          }
-
-          .arrow {
-            font-size: 10px;
-          }
-
-          .mapboxgl-popup-content {
-            width: 350px;
-            padding: 0;
-            color: #212121;
-
-            .mapboxgl-popup-close-button {
-              display: none;
-            }
-
-            .popup-content-description {
-              padding: 16px;
-              font-size: 14px;
-              line-height: 1.5;
-
-              h3 {
-                font-size: 24px;
-                line-height: 1.5;
-                font-weight: 500;
-                margin-top: 0;
-                margin-top: 16px;
-                margin-bottom: 24px;
-              }
-            }
-
-            .popup-content-footer {
-              display: flex;
-              align-items: center;
-              font-size: 18px;
-              font-weight: 400;
-              gap: 20px;
-
-              img {
-                width: 24px;
-                height: 24px;
-                margin-right: 6px;
-              }
-            }
-
-            .popup-content-footer-item {
-              display: flex;
-              align-items: center;
-            }
-
-            img {
-              width: 350px;
-              height: 120px;
-            }
-          }
-        }
-      </style>
-    `;
   }
 
   connectedCallback() {
-    // Add default styles if they haven't been added yet
-    if (!OSMap.defaultStylesAdded) {
-      document.head.insertAdjacentHTML("afterbegin", this.defaultStyles);
-
-      OSMap.defaultStylesAdded = true;
-    }
     const id = this.getAttribute("id") || "map";
     const endpoint = this.getAttribute("data-os-endpoint");
     const key = this.getAttribute("data-os-key");
@@ -197,44 +37,11 @@ export class OSMap extends HTMLElement {
     const mapZoom = this.getAttribute("data-os-map-zoom");
     const mapStyle = this.getAttribute("data-os-map-style");
 
-    this.innerHTML = `
-    <div id ="filter-type" class="dropdown">
-      <button class="dropdown-toggle">
-        PROJECT TYPE
-        <span class="arrow">▼</span>
-      </button>
-      
-      <div class="dropdown-menu">
-        <label class="dropdown-item">
-          <input type="checkbox" value="Enterprise" checked>
-          <span class="badge enterprise">ENTERPRISE</span>
-        </label>
-        
-        <label class="dropdown-item">
-          <input type="checkbox" value="Quantum Computing" checked>
-          <span class="badge quantum">QUANTUM CAMPUS</span>
-        </label>
-        
-        <label class="dropdown-item">
-          <input type="checkbox" value="AI" checked>
-          <span class="badge ai">AI</span>
-        </label>
-        
-        <label class="dropdown-item">
-          <input type="checkbox" value="Colocation" checked>
-          <span class="badge colocation">COLOCATION</span>
-        </label>
-        
-        <label class="dropdown-item">
-          <input type="checkbox" value="Hyperscale" checked>
-          <span class="badge hyperscale">HYPERSCALE</span>
-        </label>
-      </div>
-    </div>
-`;
+    this.innerHTML = filterTypeTemplate;
 
     const mapContainer = document.createElement("div");
     mapContainer.id = id;
+    mapContainer.classList.add("map");
     this.appendChild(mapContainer);
 
     mapboxgl.accessToken = key;
@@ -388,14 +195,8 @@ export class OSMap extends HTMLElement {
           });
 
           // Create an array to store the selected types
-          let originalData = data; // Store the original data
-          let selectedTypes = [
-            "Enterprise",
-            "Quantum Computing",
-            "AI",
-            "Colocation",
-            "Hyperscale",
-          ];
+          const originalData = data; // Store the original data
+          let selectedTypes = [];
 
           checkboxes.forEach((input) => {
             input.addEventListener("change", (event) => {
@@ -417,7 +218,11 @@ export class OSMap extends HTMLElement {
               };
 
               // Update the map source with filtered data
-              map.getSource("locations").setData(filteredData);
+              map
+                .getSource("locations")
+                .setData(
+                  selectedTypes.length === 0 ? originalData : filteredData,
+                );
             });
           });
 
